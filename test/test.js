b467b2d6958535199723119a88e8784404d2a919
Daniel Tibola <danieltibola@gmail.com>
2018-01-15 13:38:58 -0200

    Applying tests to produtos endpoint

--- test/test.js
+++ test/test.js
@@ -1,5 +1,37 @@
-describe('Teste Mocha', function(){
-    it('Primeira execucao', function(){
-        console.log('teste passou aqui');
+var http = require("http");
+var assert = require("assert");
+
+describe('Endpoint /produtos', function(){
+    it('#Lista produtos json', function(done){
+        var configuracoes = {
+            host: "localhost",
+            port: 3000,
+            path: "/produtos",
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+
+        http.get(configuracoes, function(res){
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.headers["content-type"], "application/json; charset=utf-8");
+            done();
+        });
+    });
+    it('#Lista produtos html', function(done){
+        var configuracoes = {
+            host: "localhost",
+            port: 3000,
+            path: "/produtos",
+            headers: {
+                "Accept": "text/html"
+            }
+        };
+
+        http.get(configuracoes, function(res){
+            assert.equal(res.statusCode, 200);
+            assert.equal(res.headers["content-type"], "text/html; charset=utf-8");
+            done();
+        });
     });
 });
