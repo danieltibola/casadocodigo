e7b14085a562d66aef90fa0797b1241ad67f7fff
Daniel Tibola <danieltibola@gmail.com>
2018-01-16 18:06:15 -0200

    Setting heroku deploy

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -17,6 +17,16 @@ function createDBConnection (){
       database: 'casadocodigo_teste'
     });
   }
+  else if(process.env.NODE_ENV === 'production'){
+    var urlConn = process.env.CLEARDB_DATABASE_URL;
+    var grupos = urlConn.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+    return mysql.createConnection({
+      host: grupos[3],
+      user: grupos[1],
+      password: grupos[2],
+      database: grupos[4]
+    });
+  }
 };
 //wrapper
 module.exports = function(){
